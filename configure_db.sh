bd55a2a167e63a88321be74e68363985dcb171e4
Nikita Zavadin <53271136+RB387@users.noreply.github.com>
2020-05-08 15:44:07 +0300

    Update configure_db.sh

--- configure_db.sh
+++ configure_db.sh
@@ -3,7 +3,6 @@ echo Enter admin password:
 read password
 psql postgres -c "CREATE USER admin WITH PASSWORD '$password';"
 echo psql postgres -c "CREATE USER admin WITH PASSWORD '$password';"
-psql postgres -c "CREATE DATABASE test_blog;"
 psql postgres -c "ALTER ROLE admin SET client_encoding TO 'utf8';"
 psql postgres -c "ALTER ROLE admin SET default_transaction_isolation TO 'read committed';"
 psql postgres -c "ALTER ROLE admin SET timezone TO 'UTC';"
